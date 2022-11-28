122f27108456a422dae91bac52e98cac24c14d98
张傲寒 <57825418+smalljavadd@users.noreply.github.com>
2022-11-29 01:38:10 +0800

    03 dice page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study_diary/02_my_info_card.dart';
+import 'package:flutter_study_diary/03_dice_page.dart';
 
 void main() {
-  runApp(const MyInfoCard());
+  runApp(const DicePage());
 }
