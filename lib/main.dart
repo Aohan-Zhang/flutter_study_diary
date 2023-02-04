c06af0a53b401547d3204a04df5bf1f7a4914244
aohan <zhangaohan@163.com>
2023-02-04 13:26:05 +0800

    04 simple piano

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study_diary/03_dice_page.dart';
+import 'package:flutter_study_diary/04_simple_piano.dart';
+
+import '01_layout_basic.dart' as cls_01;
 
 void main() {
-  runApp(const DicePage());
+  runApp(const Piano());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal[300],
+        appBar: AppBar(
+          backgroundColor: Colors.teal[900],
+          title: const Text('my flutter study diary'),
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  color: Colors.teal,
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const cls_01.MyApp()),
+                      );
+                    },
+                    child: const Text('text',
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
